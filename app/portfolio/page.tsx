231b39d51b1d98043715c084bf50abda9c17ef3f
milan <milanzivanov@users.noreply.github.com>
2025-04-13 17:52:20 +0200

    removed and commented some code

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,12 +1,12 @@
 // import { PrismaClient } from "@prisma/client";
 // const prisma = new PrismaClient();
-import db from "@/utils/db";
+// import db from "@/utils/db";
 
 export default async function PorfolioPage() {
   // const profile = await prisma.profile.findMany();
   // console.log("///////// ", profile);
 
-  console.log("/////////123 ", db.profile.findMany());
+  // console.log("/////////123 ", db.profile.findMany());
 
   return (
     <>
