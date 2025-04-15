70459446f6fb3867f2438b34bd4d897561f03178
milan <milanzivanov@users.noreply.github.com>
2025-04-15 23:53:57 +0200

    portfolio page edited

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,21 +2,29 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function PorfolioPage() {
-  const projects = await prisma.project.findMany();
+  interface Project {
+    id: string;
+    name: string;
+    description: string;
+  }
+
+  const projects: Project[] = await prisma.project.findMany();
   // console.log("///////// ", projects);
 
   return (
-    <div className="container">
-      <h1 className="text-3xl">Portfolio page</h1>
-      <ul>
-        {projects.map((p) => (
-          <li key={p.id} className="text-2xl">
-            {p.name}
-            <br />
-            {p.description}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <section className="h-[calc(100dvh-101px)] bg-slate-50 dark:bg-slate-800 py-5">
+      <div className="container">
+        <h1 className="text-3xl">Portfolio page</h1>
+        <ul>
+          {projects.map((p) => (
+            <li key={p.id} className="text-2xl">
+              {p.name}
+              <br />
+              {p.description}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 }
