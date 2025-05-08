752824d707a30c2750c79ec33007edc6e029d1ed
milan <milanzivanov@users.noreply.github.com>
2025-05-08 15:07:41 +0200

    little margin fix

--- components/portfolio/ProjectsList.tsx
+++ components/portfolio/ProjectsList.tsx
@@ -8,7 +8,7 @@ export default function ProjectsList({
   projects: ProjectCardProps[];
 }) {
   return (
-    <section className="mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
+    <section className="my-4 gap-8 grid sm:grid-cols-2  xl:grid-cols-3  2xl:grid-cols-4">
       {projects.map((project) => {
         return <ProjectCard key={project.id} project={project} />;
       })}
